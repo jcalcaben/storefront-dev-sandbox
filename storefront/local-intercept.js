1ee395bfc73569d8c679d17a31aa983635cf21f8
James Calcaben <calcaben@adobe.com>
2021-03-04 09:33:45 -0600

    Intercept Venia UI components

--- storefront/local-intercept.js
+++ storefront/local-intercept.js
@@ -17,6 +17,39 @@
  * or modify functionality from its dependencies.
  */
 
-function localIntercept(targets) {}
+// Import the Targetables manager
+const { Targetables } = require('@magento/pwa-buildpack');
+
+function localIntercept(targets) {
+    // Create a bound Targetable factory
+    const targetables = Targetables.using(targets);
+
+    // Create a React component targetable linked to the productFullDetail.js file
+    const ProductDetails = targetables.reactComponent(
+        '@magento/venia-ui/lib/components/ProductFullDetail/productFullDetail.js'
+    );
+
+    // Add an import statement to the productFullDetail.js file and
+    // return the SingleImportStatement object
+    const TagList = ProductDetails.addImport("{TagList} from 'tagList'");
+
+    // Insert the TagList component after the product description and pass in the
+    // new categoriesListData object added to the useProductFullDetails() hook
+    ProductDetails.insertAfterJSX(
+        '<RichText content={productDetails.description} />',
+        `<${TagList} categoriesListData={productDetails.categoriesListData} />`
+    );
+    
+    // Create an ES Module targetable linked to the useProductFullDetail.js file
+    const useProductFullDetails = targetables.esModule(
+        '@magento/peregrine/lib/talons/ProductFullDetail/useProductFullDetail.js'
+    );
+
+    // Wrap the useProductFullDetail hook with your extension's wrapper file
+    useProductFullDetails.wrapWithFile(
+        'useProductFullDetail',
+        'tagList/src/targets/wrapper'
+    );
+}
 
 module.exports = localIntercept;
