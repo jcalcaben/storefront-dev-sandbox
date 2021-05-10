d951f9d4a7239b34890cef68eabd875405eb1749
James Calcaben <calcaben@adobe.com>
2021-05-10 09:16:17 -0500

    Add spliceSource examples

--- storefront/local-intercept.js
+++ storefront/local-intercept.js
@@ -1,22 +1,39 @@
-/* eslint-disable */
-/**
- * Custom interceptors for the project.
- *
- * This project has a section in its package.json:
- *    "pwa-studio": {
- *        "targets": {
- *            "intercept": "./local-intercept.js"
- *        }
- *    }
- *
- * This instructs Buildpack to invoke this file during the intercept phase,
- * as the very last intercept to run.
- *
- * A project can intercept targets from any of its dependencies. In a project
- * with many customizations, this function would tap those targets and add
- * or modify functionality from its dependencies.
- */
+const { Targetables } = require('@magento/pwa-buildpack')
 
-function localIntercept(targets) {}
+module.exports = targets => {
+    const targetableFactory = Targetables.using(targets);
 
-module.exports = localIntercept;
+    // Create a TargetableModule instance that points to the main.js source
+    const MainComponent = targetableFactory.module(
+        '@magento/venia-ui/lib/components/Main/main.js'
+    );
+
+    // Insert a console log message in the source
+    MainComponent.insertAfterSource(
+        'const Main = props => {\n',
+        '\tconsole.log("Hello World");\n'
+    );
+
+  MainComponent.spliceSource({
+    after: 'export default Main;',
+    insert: ' // Exports the main component'
+  })
+
+  MainComponent.spliceSource({
+    before: 'props =>',
+    remove: 5,
+    insert: '({ children, isMasked, propClasses })'
+  })
+
+  MainComponent.spliceSource({
+    before: '    const { children, isMasked }',
+    remove: 42
+  })
+
+  MainComponent.spliceSource({
+    after: 'mergeClasses(defaultClasses, ',
+    remove: 7,
+    insert: 'propC'
+  })
+
+}
