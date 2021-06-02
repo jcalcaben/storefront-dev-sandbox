b1471dc92f8f27f3a83f1d55f6c79aa07f69f3a6
James Calcaben <calcaben@adobe.com>
2021-06-02 10:21:28 -0500

    Add small example from targetables tutorial

--- storefront/local-intercept.js
+++ storefront/local-intercept.js
@@ -1,22 +1,21 @@
-/* eslint-disable */
-/**
- * Custom interceptors for the project.
- *
- * This project has a section in its package.json:
- *    "pwa-studio": {
- *        "targets": {
- *            "intercept": "./local-intercept.js"
- *        }
- *    }
- *
- * This instructs Buildpack to invoke this file during the intercept phase,
- * as the very last intercept to run.
- *
- * A project can intercept targets from any of its dependencies. In a project
- * with many customizations, this function would tap those targets and add
- * or modify functionality from its dependencies.
- */
+const { Targetables } = require('@magento/pwa-buildpack')
 
-function localIntercept(targets) {}
+module.exports = targets => {
+    const targetables = Targetables.using(targets);
 
-module.exports = localIntercept;
+    // Create a TargetableModule instance that points to the main.js source
+    const MainComponent = targetables.module(
+        '@magento/venia-ui/lib/components/Main/main.js'
+    );
+
+    /**
+     * Example from: https://pwastudio.io/tutorials/targetables/#create-targetable-objects
+     */
+
+    // Insert a console log message in the source
+    MainComponent.insertAfterSource(
+        'const Main = props => {\n',
+        '\tconsole.log("Hello World");\n'
+    );
+
+}
